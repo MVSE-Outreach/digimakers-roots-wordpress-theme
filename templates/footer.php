9f7aa8077a4e654b08b90bd45f3b8f075a58d2ec
Will Price <will.price94@gmail.com>
2014-11-07 01:11:56 +0000

    Add footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,10 @@
 <footer class="content-info" role="contentinfo">
+  <div class="footer-top-line"></div>
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <img src="/app/themes/digimaker-roots/assets/img/footer/uob-logo.png" alt="University of Bristol Logo"></img>
+  <img src="/app/themes/digimaker-roots/assets/img/footer/bcs-logo.png" alt="BCS Logo"></img>
+  <img src="/app/themes/digimaker-roots/assets/img/footer/arm-logo.png" alt="ARM Logo"></img>
+  <div class="footer-bottom-line"></div>
 </footer>
